9f01431326b043d9a3df1a29cf2a0c0bc9dc8a72
Nabeel Wasif <113496541+nabeel-w@users.noreply.github.com>
2024-12-14 12:47:51 +0530

    Delete validation directory

--- validation/uploadValidator.js
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-
-export const generateUploadUrlSchema = Joi.object({
-    fileName: Joi.string().min(3).required().messages({
-        'string.empty': 'fileName is required',
-        'string.min': 'fileName should have a minimum length of {#limit}',
-    }),
-})
